716a4eb882b8416d8033e9e522d39310eb1a74f2
pphunyal <prashph456@gmail.com>
2025-09-23 00:17:52 +0545

    functions and parameters

---
+++ functions.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void printSomething(); // function prototype
+
+int main() 
+{
+    printSomething();
+    return 0;
+}
+
+void printSomething() 
+{
+    cout << "i am text on the screen" << endl;
+}
---
+++ parameters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void printCrap(int x)
+{
+    cout << "Bucky's favorite number is " << x << endl;
+}
+int main()
+{   
+    printCrap(19);
+    return 0;
+}
