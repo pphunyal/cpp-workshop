3ab0c484b6d3fbc42704055c0248450fa066e86b
pphunyal <prashph456@gmail.com>
2025-10-14 19:51:48 +0545

    classes revision

---
+++ area.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "square.h"
+
+using namespace std;
+
+int main(){
+  Square s;
+  return 0;
+}
---
+++ square.cpp
@@ -0,0 +1,8 @@
+#include "square.h"
+#include <iostream>
+
+using namespace std;
+
+Square::Square() {
+  cout << "I am a square" << endl;
+}
